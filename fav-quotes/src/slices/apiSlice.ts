d25781ce05dc4b5ab1d567719d0b9bcef86d6881
jesse <jessebett1999@gmail.com>
2023-09-11 16:52:41 +0300

    final update on baseUrl

--- fav-quotes/src/slices/apiSlice.ts
+++ fav-quotes/src/slices/apiSlice.ts
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 
-const baseQuery = fetchBaseQuery({ baseUrl: "https://fav-quotes-server.onrender.com/" });
+const baseQuery = fetchBaseQuery({ baseUrl: "https://fav-quotes-server.onrender.com" });
 
 export const apiSlice = createApi({
   baseQuery,
