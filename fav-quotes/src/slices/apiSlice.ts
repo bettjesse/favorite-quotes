ad9df3de892f9d7bf49bbf5510241b14ca541cbf
jesse <jessebett1999@gmail.com>
2023-09-11 17:58:29 +0300

    cookie fix

--- fav-quotes/src/slices/apiSlice.ts
+++ fav-quotes/src/slices/apiSlice.ts
@@ -2,6 +2,8 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 
 const baseQuery = fetchBaseQuery({ baseUrl: "https://fav-quotes-server.onrender.com" });
+// const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8080" });
+
 
 export const apiSlice = createApi({
   baseQuery,
